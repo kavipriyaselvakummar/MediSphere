527ada5bb09ad80bf33ec9f2107898a015897988
kavipriyaselvakummar <kavipriyaselvakummar@gmail.com>
2026-08-17 21:06:48 +0530

    Commit with static values

---
+++ src/test/java/com/example/medispherespringboot/MedispherespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medispherespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedispherespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
